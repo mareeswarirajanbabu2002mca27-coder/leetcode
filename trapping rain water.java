14dd52b0481ea54907cf2690b55fd65af7873e9e
mareeswarirajanbabu2002mca27-coder <mareeswarirajanbabu2002_mca27@mepcoeng.ac.in>
2026-04-28 16:15:17 +0530

    Create trapping rain water.java

---
+++ trapping rain water.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int trap(int[] height) {
+        if (height == null || height.length == 0) return 0;
+
+        int left = 0, right = height.length - 1;
+        int leftMax = 0, rightMax = 0;
+        int totalWater = 0;
+
+        while (left < right) {
+            if (height[left] < height[right]) {
+                // If current left height is less than right, 
+                // water is limited by the left side
+                if (height[left] >= leftMax) {
+                    leftMax = height[left];
+                } else {
+                    totalWater += leftMax - height[left];
+                }
+                left++;
+            } else {
+                // If current right height is less than or equal to left, 
+                // water is limited by the right side
+                if (height[right] >= rightMax) {
+                    rightMax = height[right];
+                } else {
+                    totalWater += rightMax - height[right];
+                }
+                right--;
+            }
+        }
+
+        return totalWater;
+    }
+}
