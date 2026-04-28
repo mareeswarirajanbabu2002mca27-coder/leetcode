2a260ac9009c5c270cbb8f7f0b4cbb1e7c6e9146
mareeswarirajanbabu2002mca27-coder <mareeswarirajanbabu2002_mca27@mepcoeng.ac.in>
2026-04-28 16:00:30 +0530

    Create sudoku solver.java

---
+++ sudoku solver.java
@@ -0,0 +1,37 @@
+class Solution {
+    public void solveSudoku(char[][] board) {
+        if (board == null || board.length == 0) return;
+        solve(board);
+    }
+
+    private boolean solve(char[][] board) {
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (board[i][j] == '.') {
+                    for (char c = '1'; c <= '9'; c++) { // Try digits 1-9
+                        if (isValid(board, i, j, c)) {
+                            board[i][j] = c; // Put digit
+
+                            if (solve(board)) return true; // If it leads to a solution
+                            else board[i][j] = '.'; // Backtrack
+                        }
+                    }
+                    return false; // No digit fits, trigger backtracking
+                }
+            }
+        }
+        return true; // All cells filled
+    }
+
+    private boolean isValid(char[][] board, int row, int col, char c) {
+        for (int i = 0; i < 9; i++) {
+            // Check row
+            if (board[i][col] == c) return false;
+            // Check column
+            if (board[row][i] == c) return false;
+            // Check 3x3 box
+            if (board[3 * (row / 3) + i / 3][3 * (col / 3) + i % 3] == c) return false;
+        }
+        return true;
+    }
+}
