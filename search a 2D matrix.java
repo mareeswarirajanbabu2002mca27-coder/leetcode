c24981c51fe0d3bfcabd84da7e3bd40203a7ed60
mareeswarirajanbabu2002mca27-coder <mareeswarirajanbabu2002_mca27@mepcoeng.ac.in>
2026-09-02 09:44:41 +0530

    Create search a 2D matrix.java

---
+++ search a 2D matrix.java
@@ -0,0 +1,32 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+            return false;
+        }
+        
+        int m = matrix.length;
+        int n = matrix[0].length;
+        
+        int low = 0;
+        int high = (m * n) - 1;
+        
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            
+            // Map the 1D mid index to 2D coordinates
+            int row = mid / n;
+            int col = mid % n;
+            int midVal = matrix[row][col];
+            
+            if (midVal == target) {
+                return true;
+            } else if (midVal < target) {
+                low = mid + 1; // Search the right half
+            } else {
+                high = mid - 1; // Search the left half
+            }
+        }
+        
+        return false;
+    }
+}
