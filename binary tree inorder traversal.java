5b539ea89f566a165841b19da38aca72048d4ce4
mareeswarirajanbabu2002mca27-coder <mareeswarirajanbabu2002_mca27@mepcoeng.ac.in>
2026-09-08 14:55:08 +0530

    Create binary tree inorder traversal.java

---
+++ binary tree inorder traversal.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        helper(root, result);
+        return result;
+    }
+
+    private void helper(TreeNode node, List<Integer> result) {
+        if (node == null) {
+            return;
+        }
+        helper(node.left, result);  // Traverse left subtree
+        result.add(node.val);       // Visit root
+        helper(node.right, result); // Traverse right subtree
+    }
+}
